f21f0d0c743f17063d91d86bec5241b5478b3922
Maxim Kobelev <74362240+sheriff1max@users.noreply.github.com>
2026-04-04 11:35:58 +0300

    Update make_network.py

--- models/make_network.py
+++ models/make_network.py
@@ -7,8 +7,8 @@ def get_network(cfg, device, net_name='MVP_FAS'):
     net = torch.nn.DataParallel(net).to(device)
     return net
 
-def set_pretrained_setting(net,optimizer,weight_path):
-    checkpoint_dict = torch.load(weight_path)
+def set_pretrained_setting(net, optimizer, weight_path, device):
+    checkpoint_dict = torch.load(weight_path, map_location=device)
     checkpoint = checkpoint_dict['state_dict']
     optim_checkpoint = checkpoint_dict['optimizer']
     last_epoch = checkpoint_dict['epoch']-1
